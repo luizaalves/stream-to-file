85b0a6c58947e9b29f27057e57b718ca65a80555
Luiza <l.uhzinha@hotmail.com>
2024-10-08 09:40:06 -0300

    Change files between server and client

--- client/client.cpp
+++ client/client.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <sys/socket.h>
-
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstring>
 #include "client.hpp"
 
 using namespace std;
@@ -11,6 +13,22 @@ Client::Client(void) {
 
 void Client::connect_server(void) {
     cout << "Hello, I'm Client" << endl;
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    cout << server_fd << endl;
+    if(server_fd == -1) return;
+
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    char buffer[1024] = "Enviando mensagem pro servidor...";
+    cout << buffer << endl;
+
+    int ret = connect(server_fd, (struct sockaddr *) &server_addr, (socklen_t) sizeof(server_addr));
+    cout << ret << endl;
+
+    ret = send(server_fd, &buffer, strlen(buffer), 0);
+    cout << ret << endl;
 }
 
 Client::~Client() {
--- server/server.cpp
+++ server/server.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 #include "server.hpp"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <fstream>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
 
 using namespace std;
 
@@ -8,7 +15,48 @@ Server::Server(void) {
 }
 
 void Server::connect(void) {
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     cout << "Hello, I'm Server" << endl;
+    if(server_fd == -1) return;
+
+    sockaddr_in addr;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8080);
+
+    int ret = bind(server_fd, (struct sockaddr *)&addr, sizeof(addr));
+    if(ret == -1) return;
+
+    ret = listen(server_fd, 5);
+    if(ret == -1) return;
+
+    cout << "Waiting conections" << endl;
+
+    socklen_t addrlen = sizeof(addr);
+    int client_socket = accept(server_fd, (struct sockaddr *) &addr, &addrlen);
+    if(client_socket == -1) return;
+
+    cout << "Connection established" << endl;
+    char buffer[1024] = {0};
+    ret = recv(client_socket, buffer, 1024, 0);
+    if(ret == -1) return;
+    time_t timestamp;
+    time(&timestamp);
+    tm* local_time = localtime(&timestamp);
+    ostringstream oss;
+    oss << put_time(local_time, "%Y%m%d%H%M%S");
+    
+    string nome_arquivo = "PREFIXO_" + oss.str();
+    ofstream outfile("../server/"+nome_arquivo);
+    if (!outfile) return;
+
+    outfile << buffer;
+    outfile.close();
+
+    close(client_socket);
+    close(server_fd);
+
+    return;
 }
 
 Server::~Server() {
