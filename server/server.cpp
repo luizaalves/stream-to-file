3d5a243671d0ba6b330742e6162904f42506d84c
Luiza <l.uhzinha@hotmail.com>
2024-10-08 13:14:17 -0300

    Server saving files received

--- server/server.cpp
+++ server/server.cpp
@@ -1,5 +1,10 @@
 #include "server.hpp"
 #include <boost/array.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include <iostream>
+#include <iomanip>
+
+namespace fs = boost::filesystem;
 
 void Server::connect(void) {
     try {
@@ -10,12 +15,22 @@ void Server::connect(void) {
             boost::system::error_code error;
 
             size_t len = socket.read_some(boost::asio::buffer(buf), error);
+            if(len == 0) continue;
             if (error == boost::asio::error::eof)
             break; // Connection closed cleanly by peer.
             else if (error)
                 throw boost::system::system_error(error); // Some other error.
+            time_t timestamp;
+            time(&timestamp);
+            tm* local_time = localtime(&timestamp);
+            ostringstream oss;
+            oss << put_time(local_time, "%Y%m%d%H%M%S");
+            
+            string nome_arquivo = "PREFIXO_" + oss.str();
 
-            cout << buf.data() << endl;
+            fs::path p{"../server/"+nome_arquivo};
+            fs::ofstream ofs{p};
+            ofs.write(buf.data(), len);
         }
     } catch (exception& e)
     {
