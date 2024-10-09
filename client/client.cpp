4b86cd40f72d7bc48b5dfa37c50a9462bd05f843
Luiza <l.uhzinha@hotmail.com>
2024-10-09 16:58:25 -0300

    Refactor client function to be clearer

--- client/client.cpp
+++ client/client.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 
 #include "client.hpp"
+#define BUF_SIZE 512
 
 using namespace std;
 namespace fs = boost::filesystem;
@@ -14,24 +15,29 @@ using boost::filesystem::path;
 void Client::connect_server(void) {
     try {
         boost::asio::connect(socket, endpoints);
-        uint8_t buf[512];
+        uint8_t buf[BUF_SIZE];
         size_t len;
-        fs::path p{file_to_send};
-        fs::ifstream ofs{p};
+        uint16_t bytes_read = BUF_SIZE;
+        int blocks_read_from_file = 0;
+
+        fs::path path{file_to_send};
+        fs::ifstream ofs{path};
         if(!ofs) return;
-        uint16_t bytes_read=512;
-        int i=0;
-        while(bytes_read==512) {
-            ofs.seekg(i);
+        
+        while(bytes_read == BUF_SIZE) {
+            ofs.seekg(blocks_read_from_file);
             ofs.read((char *)buf, sizeof(buf));
+
             boost::system::error_code ignored_error;
+
             boost::asio::write(socket, boost::asio::buffer(buf, ofs.gcount()), ignored_error);
-            if(ofs.gcount() == 512){
-                i+=512;
+
+            if(ofs.gcount() == BUF_SIZE){
+                blocks_read_from_file+=BUF_SIZE;
             }else {
                 bytes_read = ofs.gcount();
             }
-            memset(buf, 0, 512); 
+            memset(buf, 0, BUF_SIZE); 
         }
         cout << "Message sent" << endl;
 
