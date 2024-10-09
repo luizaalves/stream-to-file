1be41cbe59513ca65437b69760356f8886d2a5f0
Luiza <l.uhzinha@hotmail.com>
2024-10-09 16:53:02 -0300

    Refactor function to get file name be clearer

--- server/server.cpp
+++ server/server.cpp
@@ -14,32 +14,26 @@ void Server::connect(void) {
             boost::array<uint8_t, 128> buf;
             boost::system::error_code error;
 
-            time_t timestamp;
-            time(&timestamp);
-            tm* local_time = localtime(&timestamp);
-            ostringstream oss;
-            oss << put_time(local_time, "%Y%m%d%H%M%S");
-            
-            string nome_arquivo = "PREFIXO_" + oss.str();
+            string nome_arquivo = get_file_name();
             string original_path = string(BASE_PATH) +"/../server/"+nome_arquivo;
             string dst_full_path = original_path;
+            int file_sequence = 1;
+            uint16_t total_size_file = 0;
+
             fs::path file_path(dst_full_path);
             
             fs::fstream ofs( file_path, ios_base::out );
 
-            int file_sequence = 1;
-            uint16_t total_size_file = 0;
-
             while(error != boost::asio::error::eof) {
-                size_t len = socket.read_some(boost::asio::buffer(buf), error);
-                if(len==0) break;
-                total_size_file+=len;
+                size_t len_read = socket.read_some(boost::asio::buffer(buf), error);
+                if(len_read==0) break;
+                total_size_file+=len_read;
                 if (error && error != boost::asio::error::eof)
                     throw boost::system::system_error(error); // Some other error.
                 
                 if(total_size_file > size_file_max) {
                     size_t size_new_file = total_size_file-size_file_max;
-                    size_t new_pos = len-size_new_file;
+                    size_t new_pos = len_read-size_new_file;
 
                     ofs.write((char *) buf.data(), new_pos);
                     
@@ -53,7 +47,7 @@ void Server::connect(void) {
                     ofs.write((char *) buf.data()+new_pos, size_new_file);
                     total_size_file=size_new_file;
                 }
-                else ofs.write((char *) buf.data(), len);
+                else ofs.write((char *) buf.data(), len_read);
                 memset(buf.data(), 0, buf.size()); 
             }
         }
@@ -67,4 +61,15 @@ void Server::connect(void) {
 
 Server::~Server() {
     //Destructor    
+}
+
+string Server::get_file_name(void) {
+    time_t timestamp;
+    time(&timestamp);
+    tm* local_time = localtime(&timestamp);
+    ostringstream oss;
+    oss << put_time(local_time, "%Y%m%d%H%M%S");
+    
+    string file_name = "PREFIXO_" + oss.str();
+    return file_name;
 }
--- server/server.hpp
+++ server/server.hpp
@@ -13,6 +13,8 @@ private:
     boost::asio::io_context io_context;
     tcp::acceptor acceptor;
     uint16_t size_file_max;
+
+    static string get_file_name(void);
 public:
 Server(unsigned short port_num, uint16_t file_size) 
 : acceptor(io_context, tcp::endpoint(tcp::v4(), port_num)) {
